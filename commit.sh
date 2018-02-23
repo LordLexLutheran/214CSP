8bf84b10ae65a9592d4a6254534ba45d486db0f6
Alex Miget <alexmiget11@gmail.com>
2018-02-23 14:03:55 +0000

    Quick Commit

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 git add .
 git commit -m "Quick Commit"
-git push https://github.com/LordLexLutheran/214CSP.git/ --all
+git push https://LordLexLutheran:Dragon11@github.com/LordLexLutheran/214CSP.git/ --all
